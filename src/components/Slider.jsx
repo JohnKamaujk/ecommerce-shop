14fd2a146fa65e13f91e60105574d51e77766551
JohnKamaujk <johnjkkamau@gmail.com>
2022-08-08 16:08:06 +0300

    Hardcoded two more slides & styled

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -26,12 +27,14 @@ const Arrow = styled.div`
 `;
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
 `;
 const Slide = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 const ImgContainer = styled.div`
   height: 100%;
@@ -44,9 +47,21 @@ const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
 `;
-const Title = styled.h1``;
-const Description = styled.p``;
-const Button = styled.button``;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
   return (
@@ -55,12 +70,36 @@ const Slider = () => {
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper>
-        <Slide>
+        <Slide bg="f5fafd">
           <ImgContainer>
             <Image src="https://i.ibb.co/cXFnLLV/3.png" />
           </ImgContainer>
           <InfoContainer>
             <Title>SUMMER SALE</Title>
+            <Description>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Description>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/cXFnLLV/3.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Winter SALE</Title>
+            <Description>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Description>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f4">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/cXFnLLV/3.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Autumn SALE</Title>
             <Description>
               DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
             </Description>
